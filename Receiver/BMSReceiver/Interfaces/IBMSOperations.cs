b1ec35c3d1fc375cb76d4d935cfdf85a1d26bf64
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-06-12 20:03:32 +0530

    Create IBMSOperations.cs

---
+++ Receiver/BMSReceiver/Interfaces/IBMSOperations.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMSReceiver.Interfaces
+{
+    public interface IBMSOperations
+    {
+        double FindMaximum(List<double> measures);
+        double FindMinimum(List<double> measures);
+        double FindSimpleMovingAverage(List<double> measures);
+        void PrintOperationResult(MeasureResult result);
+    }
+}
